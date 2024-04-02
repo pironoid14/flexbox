b452ca578c4597c632aa9c2ae37e18a8f598420a
pironoid14 <peteremieje@gmail.com>
2024-04-02 08:17:53 +0100

    assignment

---
+++ src/flexbox.jsx
@@ -0,0 +1,36 @@
+import React from "react"; 
+import './flexbox.css';
+
+function flexbox(){
+return(
+    <div className="flex-container">
+  <div className="container-flex">
+
+  <h1 className="flex"> shola</h1>
+  <h1 className="flex">bolanle</h1>
+  <h1 className="flex">chinonso</h1>
+  </div>
+  
+  <div className="grid-container">
+  <h1 className="tab1"> shola</h1>
+  <h1 className="tab2">bolanle</h1>
+  <h1 className="tab3">chinonso</h1> 
+  <h1 className="tab4">great</h1>
+  <h1 className="tab5">runor</h1>
+  <h1 className="tab6">victor</h1>
+  <h1 className="tab7">Israel</h1>
+  <h1 className="tab8">mercy</h1>
+  <h1 className="tab9">chibogu</h1>
+  <h1 className="tab10">chinenye</h1>
+  <h1 className="tab11">chidinma</h1>
+  <h1 className="tab12">kola</h1>
+  <h1 className="tab13">alele</h1>
+  <h1 className="tab14">beals</h1>
+  <h1 className="tab15">joshua</h1>
+  <h1 className="tab16">thomas</h1>
+  </div>
+  </div>
+)
+
+}
+export default flexbox
